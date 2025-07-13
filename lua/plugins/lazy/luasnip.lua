99c17069450974d1b7df781b53ab434b887da104
Bruno Guimarães Souza <brunoguimaraes.guigui@gmail.com>
2025-07-13 14:13:08 -0300

    feat: Add copilot and a fancy scroll

--- lua/plugins/lazy/luasnip.lua
+++ lua/plugins/lazy/luasnip.lua
@@ -1,7 +1,57 @@
 return {
-	"L3MON4D3/LuaSnip",
-	-- follow latest release.
-	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-	-- install jsregexp (optional!).
-	build = "make install_jsregexp"
+        "L3MON4D3/LuaSnip",
+        -- follow latest release.
+        version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+        -- install jsregexp (optional!).
+        build = "make install_jsregexp",
+
+        config = function()
+
+        local luasnip = require("luasnip")
+                local cmp = require("cmp")
+
+                cmp.setup({
+                        mapping = {
+
+                                -- ... Your other mappings ...
+                                ['<CR>'] = cmp.mapping(function(fallback)
+                                        if cmp.visible() then
+                                                if luasnip.expandable() then
+                                                        luasnip.expand()
+                                                else
+                                                        cmp.confirm({
+                                                                select = true,
+                                                        })
+                                                end
+                                        else
+                                                fallback()
+                                        end
+                                end),
+
+                                ["<Tab>"] = cmp.mapping(function(fallback)
+                                        if cmp.visible() then
+                                                cmp.select_next_item()
+                                        elseif luasnip.locally_jumpable(1) then
+                                                luasnip.jump(1)
+                                        else
+                                                fallback()
+                                        end
+                                end, { "i", "s" }),
+
+                                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                                        if cmp.visible() then
+                                                cmp.select_prev_item()
+                                        elseif luasnip.locally_jumpable(-1) then
+                                                luasnip.jump(-1)
+                                        else
+                                                fallback()
+                                        end
+                                end, { "i", "s" }),
+
+                                -- ... Your other mappings ...
+                        },
+
+                        -- ... Your other configuration ...
+                })
+        end,
 }
--- lua/plugins/map.lua
+++ lua/plugins/map.lua
@@ -2,6 +2,20 @@
 vim.keymap.set('n', '<leader>n', ':enew<CR>', { desc = 'Create new file' })
 vim.keymap.set('n', '<C-s>', ':w<CR>', { desc = 'Save file' })
 vim.keymap.set('n', '<leader>q', ':q<CR>', { desc = 'Close neovim' })
+vim.keymap.set('n', '<C-a>', 'ggvG', { desc = 'Close neovim' })
+
+-- Move lines
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==")
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==")
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv")
+
+-- System clipboard
+vim.keymap.set('n', '<C-c>', '"+y', { desc = "Yank to system clipboard" })
+vim.keymap.set('n', '<C-V>', '"+p', { desc = "Paste from system clipboard" })
+vim.keymap.set('v', '<C-c>', '"+y', { desc = "Yank to system clipboard" })
+vim.keymap.set('v', '<C-V>', '"+p', { desc = "Paste from system clipboard" })
+vim.keymap.set('i', '<C-V>', '<ESC>"+pi', { desc = "Paste from system clipboard" })
 
 -- Split window
 vim.keymap.set('n', '<leader>-', ':split<CR>', { desc = 'Split window horizontally' })
@@ -37,11 +51,13 @@ vim.keymap.set('n', '<leader>v', ':ToggleTerm size=40 direction=vertical dir=%:p
 vim.keymap.set('t', '<C-x>', [[<C-\><C-n>]], { desc = "Exit terminal mode" })
 
 -- Dap
-vim.keymap.set('n', '<leader>bb', ':DapToggleBreakpoint<CR>', { desc = "Toggle breakpoint" })
-vim.keymap.set('n', '<F1>', ':DapContinue<CR>', { desc = "Dap continue" })
-vim.keymap.set('n', '<F2>', ':DapStepInto<CR>', { desc = "Dap step into" })
-vim.keymap.set('n', '<F3>', ':DapStepOver<CR>', { desc = "Dap step over" })
-vim.keymap.set('n', '<F4>', ':DapStepOut<CR>', { desc = "Dap step out" })
+vim.keymap.set('n', '<leader>db', ':DapToggleBreakpoint<CR>', { desc = "Toggle breakpoint" })
+vim.keymap.set('n', '<leader>da', ':DapClearBreakpoints<CR>', { desc = "Clear breakpoints" })
+vim.keymap.set('n', '<F1>', ':DapContinue<CR>', { desc = "Continue" })
+vim.keymap.set('n', '<F2>', ':DapStepInto<CR>', { desc = "Step into" })
+vim.keymap.set('n', '<F3>', ':DapStepOver<CR>', { desc = "Step over" })
+vim.keymap.set('n', '<F4>', ':DapStepOut<CR>', { desc = "Step out" })
+vim.keymap.set('n', '<F6>', ':DapTerminate<CR>', { desc = "Stop" })
 
 -- Rust
 vim.keymap.set('n', '<leader>rr', ':RustLsp runnables<CR>', { desc = "Rust run" })
